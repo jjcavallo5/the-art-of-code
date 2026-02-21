c58a910314b346f3ecffca574fe67328d20f227e
Jeremy Cavallo <jcbball55@gmail.com>
2026-02-21 09:53:23 -0500

    Dataset struct for easier loading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 // Operators: https://doc.rust-lang.org/std/ops/index.html
 
-use std::mem;
 use std::{
     fs::{self, File},
     io,
@@ -22,7 +21,6 @@ fn get_files() {
         let file = files[idx];
 
         if fs::exists(file).expect("Failed to check file") {
-            println!("File {file} exists. Skipping...");
             continue;
         }
 
@@ -38,42 +36,75 @@ fn get_files() {
     }
 }
 
-fn read_train_images() -> io::Result<()> {
-    let f = File::open("train_images").unwrap();
-    let mut reader = io::BufReader::new(f);
+struct Dataset {
+    img_reader: io::BufReader<File>,
+    lbl_reader: io::BufReader<File>,
+    index: u32,
+}
+
+impl Dataset {
+    fn next(&mut self) -> ([u8; 28 * 28], u8) {
+        let mut img = [0; 28 * 28];
+        let mut lbl = [0];
+
+        self.img_reader
+            .read(&mut img)
+            .expect("Failed to read image");
+        self.lbl_reader
+            .read(&mut lbl)
+            .expect("Failed to read label");
+        self.index += 1;
+
+        return (img, lbl[0]);
+    }
+}
+
+fn get_dataset(is_test: bool) -> Dataset {
+    let mut split = "train";
+    if is_test {
+        split = "test";
+    }
+
+    // Open train image file
+    let fp_images = File::open(format!("{split}_images")).unwrap();
+    let mut img_reader = io::BufReader::new(fp_images);
+
+    // Skip past header
     let mut buffer = [0; 16];
-    reader.read(&mut buffer).unwrap(); // Header, don't care
+    img_reader.read(&mut buffer).unwrap();
 
-    let mut first_image = [0; 28 * 28];
-    reader.read(&mut first_image).unwrap();
+    // Open train label file
+    let fp_labels = File::open(format!("{split}_labels")).unwrap();
+    let mut lbl_reader = io::BufReader::new(fp_labels);
 
+    // Skip past header
+    let mut buffer = [0; 8];
+    lbl_reader.read(&mut buffer).unwrap(); // Header, don't care
+
+    return Dataset {
+        img_reader,
+        lbl_reader,
+        index: 0,
+    };
+}
+
+fn print_sample(img: [u8; 28 * 28], lbl: u8) {
     for row in 0..28 {
         for col in 0..28 {
-            let val = first_image[row * 28 + col];
+            let val = img[row * 28 + col];
             print!("{val} ");
         }
         println!();
     }
-
-    Ok(())
-}
-
-fn read_train_labels() {
-    let f = File::open("train_labels").unwrap();
-    let mut reader = io::BufReader::new(f);
-    let mut buffer = [0; 8];
-    reader.read(&mut buffer).unwrap(); // Header, don't care
-
-    let mut first_label = [0; 1];
-    reader.read(&mut first_label).unwrap();
-
-    let lbl = first_label[0];
-    println!("Label: {lbl}")
+    println!("Label: {lbl}");
 }
 
 fn main() {
-    println!("Hello world!");
     get_files();
-    read_train_images();
-    read_train_labels();
+    let mut train_dataset = get_dataset(false);
+    let mut test_dataset = get_dataset(true);
+    let (img, label) = train_dataset.next();
+    print_sample(img, label);
+    let (img_2, label_2) = test_dataset.next();
+    print_sample(img_2, label_2);
 }
