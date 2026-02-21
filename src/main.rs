28cc489744903a208f7de236e32bae52ec65bcc9
Jeremy Cavallo <jcbball55@gmail.com>
2026-02-21 14:37:01 -0500

    Backward not working, but compiles

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 // Operators: https://doc.rust-lang.org/std/ops/index.html
 
+use std::collections::hash_map::RandomState;
+use std::hash::{BuildHasher, Hash, Hasher};
+use std::ops::{Add, Div, Mul, Neg, Sub};
+use std::time::{SystemTime, UNIX_EPOCH};
 use std::{
     fs::{self, File},
     io,
     io::prelude::*,
     process::Command,
 };
+
+fn random() -> u64 {
+    let hasher = RandomState::new().build_hasher();
+    return hasher.finish();
+}
 
 fn get_files() {
     let urls = [
@@ -99,6 +108,82 @@ fn get_dataset(is_test: bool) -> Dataset {
     };
 }
 
+#[derive(Debug, Clone)]
+struct Value {
+    id: u64,
+    value: f32,
+    grad: f32,
+    children: Vec<Value>,
+    local_grads: Vec<f32>,
+}
+
+impl Add for Value {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        return Self {
+            id: random(),
+            value: self.value + other.value,
+            grad: 0.0,
+            children: vec![self, other],
+            local_grads: vec![1.0, 1.0],
+        };
+    }
+}
+
+impl Mul for Value {
+    type Output = Self;
+    fn mul(self, other: Self) -> Self {
+        return Self {
+            id: random(),
+            value: self.value * other.value,
+            grad: 0.0,
+            local_grads: vec![other.value, self.value],
+            children: vec![self, other],
+        };
+    }
+}
+
+impl Value {
+    fn new(value: f32) -> Self {
+        return Self {
+            id: random(),
+            value,
+            grad: 0.0,
+            local_grads: Vec::new(),
+            children: Vec::new(),
+        };
+    }
+
+    fn backward(mut self) {
+        let mut topo: Vec<Value> = Vec::new();
+        let mut visited_ids: Vec<u64> = Vec::new();
+        self.grad = 1.0;
+        build_topo(&mut topo, &mut visited_ids, self.clone());
+        topo.reverse();
+
+        for top in topo.clone() {
+            println!("{}:{}", top.value, top.grad);
+        }
+
+        for mut v in topo {
+            for idx in 0..v.children.len() {
+                println!("grads: {}, {}", v.local_grads[idx], v.grad);
+                v.children[idx].grad += v.local_grads[idx] * v.grad;
+            }
+        }
+    }
+}
+
+fn build_topo(topo: &mut Vec<Value>, visited_ids: &mut Vec<u64>, v: Value) {
+    if !visited_ids.contains(&v.id) {
+        visited_ids.push(v.id);
+        for child in &v.children {
+            build_topo(topo, visited_ids, child.clone());
+        }
+        topo.push(v);
+    }
+}
+
 fn print_sample(img: [f32; 28 * 28], lbl: u8) {
     for row in 0..28 {
         for col in 0..28 {
@@ -114,5 +199,12 @@ fn main() {
     let mut train_dataset = get_dataset(false);
     let mut test_dataset = get_dataset(true);
     let (img, lbl) = train_dataset.next();
-    print_sample(img, lbl)
+    print_sample(img, lbl);
+
+    let a = Value::new(2.0);
+    let b = Value::new(3.0);
+    let c = a.clone() * b.clone();
+    let L = c.clone() + a.clone();
+    L.backward();
+    println!("grad: {}, val: {}", a.grad, c.grad);
 }
